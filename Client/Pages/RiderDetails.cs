eac943f0a767c0b4074c339a4f03c0db4cdcf60d
Shalaahuddien Daffa <77540526+Shalaahuddien@users.noreply.github.com>
2023-03-08 09:22:30 +0700

    Learn

--- Client/Pages/RiderDetails.cs
+++ Client/Pages/RiderDetails.cs
@@ -1,6 +1,42 @@
+using FormulaOne.Client.Services;
+using FormulaOne.Shared.Models;
+using Microsoft.AspNetCore.Components;
+
 namespace FormulaOne.Client.Pages;
 
 public partial class RiderDetails
 {
-    
+    protected string Message = string.Empty;
+
+    protected Rider rider {get; set;} = new Rider();
+
+    [Parameter]
+    public string Id {get; set;}
+
+    [Inject]
+    private IRiderService riderService {get; set;}
+
+    [Inject]
+    private NavigationManager navigationManager {get; set;}
+
+    protected async override Task OnInitializedAsync()
+    {
+        if(string.IsNullOrEmpty(Id))
+        {
+            //Adding a new rider
+
+        }
+        else
+        {
+            //Updating a new rider
+            var riderId = Convert.ToInt32(Id);
+
+            var apiRider = await riderService.GetRider(riderId);
+
+            if(apiRider != null)
+                rider = apiRider;
+
+        }
+    }
+
 }
